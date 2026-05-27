a7f9e2d22eb89da442fc8e7aad9e53b937fc9b83
Frank Angelo Malubag <frankabad189@gmail.com>
2026-05-28 00:24:33 +0800

    feat: add authentication and authorization router

--- northstar-api/src/routes/auth.routes.ts
+++ northstar-api/src/routes/auth.routes.ts
@@ -18,10 +18,6 @@ router.get("/admin", authMiddleware, requireRole("admin"), (_, res) => {
   res.json({ message: "Welcome Admin" });
 });
 
-const token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OCwiZW1haWwiOiJmcmVzaEBub3J0aHN0YXIuY29tIiwicm9sZSI6ImVtcGxveWVlIiwiaWF0IjoxNzc5ODkyMTc4LCJleHAiOjE3Nzk5Nzg1Nzh9.Jz48cgTtxiZ0UiUlskOvpSCDZN5ngL7vK1C3OupgvHs";
-console.log(token);
-
 export default router;
 
 // the middleware works !
