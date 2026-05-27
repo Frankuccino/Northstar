23e9849e60691f3b8cb06f0cf9ac3f933616717b
Frank Angelo Malubag <frankabad189@gmail.com>
2026-05-27 23:39:51 +0800

    feat(controllers): add register and login controller
    - register controller ensures data from the request body is handled
    asynchronously in the auth.service's register function
    - login controller ensures login data is operated & validated
    asynchronously through the auth.service's login function handler that
    returns the user data without the password, and the jwt token.

---
+++ northstar-api/src/controllers/auth.controller.ts
@@ -0,0 +1,37 @@
+// Register Controller | Login Controller | Me Controller (protected route)
+import { login, register } from "../services/auth.service.js";
+import { Request, Response, NextFunction } from "express";
+
+export const registerController = async (req: Request, res: Response) => {
+  try {
+    const { email, password, name } = req.body;
+
+    const user = await register(email, password, name);
+
+    return res.json(user);
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return res.status(400).json({ error: err.message });
+    }
+    return res.status(400).json({ error: "An unknown error occured" });
+  }
+};
+
+export const loginController = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    const result = await login(email, password);
+
+    return res.json(result);
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return res.status(400).json({ error: err.message });
+    }
+    return res.status(400).json({ error: "An unknown error occured" });
+  }
+};
+
+export const meController = (req: Request, res: Response) => {
+  return res.json((req as any).user);
+};
