9d7200c99136e744073e1fcfb512506cefe113f5
Frank Angelo Malubag <frankabad189@gmail.com>
2026-05-27 23:43:44 +0800

    feat: add /auth middleware for the register and login routes

--- northstar-api/src/index.ts
+++ northstar-api/src/index.ts
@@ -2,62 +2,67 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import { db } from "./db/index.js"
+import { db } from "./db/index.js";
 import { users } from "./db/schema.js";
+import authRoutes from "./routes/auth.routes.js";
+import { InferInsertModel } from "drizzle-orm";
+import { workerData } from "worker_threads";
+
+type NewUser = InferInsertModel<typeof users>;
 
 const app = express();
-
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/auth", authRoutes);
+
 app.get("/", (_, res) => {
-    res.json({
-        message: "Northstar API running",
-    })
+  res.json({
+    message: "Northstar API running",
+  });
 });
 
 app.get("/db-test", async (_, res) => {
-    try {
-        const result = await db.execute("SELECT NOW()");
+  try {
+    const result = await db.execute("SELECT NOW()");
 
-        res.json(result);
-    } catch (error) {
-        console.error(error);
+    res.json(result);
+  } catch (error) {
+    console.error(error);
 
-        res.status(500).json({
-            error: "Database connection failed",
-        })
-    }
-})
+    res.status(500).json({
+      error: "Database connection failed",
+    });
+  }
+});
 
 app.get("/users", async (_, res) => {
-    const allUsers = await db.select().from(users);
+  const allUsers = await db.select().from(users);
 
-    res.json(allUsers);
-})
+  res.json(allUsers);
+});
 
 app.post("/users", async (req, res) => {
-    const { email, name } = req.body;
+  const { email, name } = req.body;
 
-    const newUser = await db
-        .insert(users)
-        .values({
-            email,
-            name
-        })
-        .returning();
-    res.json(newUser);
-})
-
+  const newUser = await db
+    .insert(users)
+    .values({
+      email,
+      name,
+    } as NewUser)
+    .returning();
+  res.json(newUser);
+});
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-})
-
+  console.log(`Server running on port ${PORT}`);
+});
 
 // we;ll add middlewares later, install zod, morgan, and refactor backend architecture with:
 // routes, controllers, services, db, and middleware.
+//
 //
