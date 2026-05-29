a7fcea38ba259cd31472083ea0474a9c4a335721
Frank Angelo Malubag <frankabad189@gmail.com>
2026-05-29 09:42:19 +0800

    feat: add the database operations for the controllers on employee service.
    This features 3 new database service functions:
    - getById(id) returns the specific record data using the id it accepts.
    - update(id) mutates the database column(s) using the id with the passed
    new values for the column(s), and returns updated record
    - remove(id) specifically deletes the database record for a specific id
    it accepted.

--- northstar-api/src/services/employee.service.ts
+++ northstar-api/src/services/employee.service.ts
@@ -1,5 +1,5 @@
 // Employee Service Layer (Database)
-
+import { eq } from "drizzle-orm";
 import { db } from "../db/index.js";
 import { employees } from "../db/schema.js";
 import { CreateEmployeeDTO } from "../types/employee.types.js";
@@ -12,4 +12,34 @@ export const create = async (data: CreateEmployeeDTO) => {
   const [employee] = await db.insert(employees).values(data).returning();
 
   return employee;
+};
+
+export const getById = async (id: number) => {
+  const employee = await db.query.employees.findFirst({
+    where: (employees, { eq }) => eq(employees.id, id),
+  });
+
+  return employee;
+};
+
+export const update = async (id: number, data: any) => {
+  const [updated] = await db
+    .update(employees)
+    .set({
+      ...data,
+      updatedAt: new Date(),
+    })
+    .where(eq(employees.id, id))
+    .returning();
+
+  return updated;
+};
+
+export const remove = async (id: number) => {
+  const [deleted] = await db
+    .delete(employees)
+    .where(eq(employees.id, id))
+    .returning();
+
+  return deleted;
 };
