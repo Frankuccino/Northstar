7c61d20b59cb5a83e23221e352c2bac32c7900ab
Frank Angelo Malubag <frankabad189@gmail.com>
2026-05-29 09:30:14 +0800

    feat: add controllers/route handler functions.
    - getEmployeeById returns a specific employee details on id
    - updateEmployee updates information on a specific employee on id
    - deleteEmployee deletes a specific employee by id

--- northstar-api/src/controllers/employee.controller.ts
+++ northstar-api/src/controllers/employee.controller.ts
@@ -22,3 +22,53 @@ export const createEmployee = async (req: Request, res: Response) => {
     res.status(500).json({ message: "Failed to create employee" });
   }
 };
+
+export const getEmployeeById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const employee = await employeeService.getById(Number(id));
+
+    if (!employee) {
+      return res.status(404).json({ message: "Employee not found" });
+    }
+    res.status(200).json(employee);
+  } catch (err) {
+    res.status(500).json({ message: "Failed to fetch employee " });
+  }
+};
+
+export const updateEmployee = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const updated = await employeeService.update(Number(id), req.body);
+
+    if (!updated) {
+      return res.status(404).json({ message: "Employee not found" });
+    }
+    res.status(200).json(updated);
+  } catch (err) {
+    return res.status(500).json({ message: "Failed to update employee" });
+  }
+};
+
+export const deleteEmployee = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const deleted = await employeeService.remove(Number(id));
+    if (!deleted) {
+      return res.status(404).json({
+        messag: "Employee not found",
+      });
+    }
+
+    return res.status(200).json({
+      message: "Employee deleted succesfully",
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: "Failed to delete employee",
+    });
+  }
+};
