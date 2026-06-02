341ecb147797db512ac2d6c6f58ce268c15e7b3f
Frank Angelo Malubag <frankabad189@gmail.com>
2026-06-02 22:03:44 +0800

    feat(router): configure nested route architecture

--- northstar-web/src/app/routes/index.tsx
+++ northstar-web/src/app/routes/index.tsx
@@ -1,14 +1,48 @@
 import { LoginPage } from "@/features/auth/pages/login-page";
 import { RegisterPage } from "@/features/auth/pages/register-page";
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import { AuthLayout } from "../layouts/auth-layout";
+import { PublicRoute } from "../public-route";
+import { ProtectedRoute } from "../protected-route";
+import { DashboardLayout } from "../layouts/dashboard-layout";
+import DashboardPage from "@/features/auth/pages/dashboard-page";
 
 export const router = createBrowserRouter([
   {
-    path: "/login",
-    element: <LoginPage />,
+    path: "/",
+    element: <Navigate to="/login" replace />,
   },
   {
-    path: "/register",
-    element: <RegisterPage />,
+    element: <PublicRoute />,
+    children: [
+      {
+        element: <AuthLayout />,
+        children: [
+          {
+            path: "/login",
+            element: <LoginPage />,
+          },
+          {
+            path: "/register",
+            element: <RegisterPage />,
+          },
+        ],
+      },
+    ],
+  },
+
+  {
+    element: <ProtectedRoute />,
+    children: [
+      {
+        element: <DashboardLayout />,
+        children: [
+          {
+            path: "/dashboard",
+            element: <DashboardPage />,
+          },
+        ],
+      },
+    ],
   },
 ]);
