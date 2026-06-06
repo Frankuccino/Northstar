3a63d58379a225fc876959bf443a425d6283a0d0
Frank Angelo Malubag <frankabad189@gmail.com>
2026-06-06 19:21:07 +0800

    chore: enable the baseline securities in global middleware (security header, cors, rate limiting)
    - helmet for secure HTTP headers (against clickjacking, MIME sniffing,
    etc.)
    - CORS for restricting cross-origin browser access.
    - Rate limiting to mitigate abuse and brute-force attacks
    
    security middlewares are also configured to be bypassed with development
    tests

--- northstar-api/src/app.ts
+++ northstar-api/src/app.ts
@@ -1,22 +1,19 @@
 import "dotenv/config";
 import express from "express";
-import cors from "cors";
-import { db } from "./db/index.js";
-import { users } from "./db/schema.js";
+
 import authRoutes from "./routes/auth.routes.js";
-import { InferInsertModel } from "drizzle-orm";
 import employeeRoutes from "./routes/employee.routes.js";
 
-type NewUser = InferInsertModel<typeof users>;
+import { securityHeaders } from "./middleware/security.middleware.js";
+import { corsOptions } from "./middleware/cors.middleware.js";
+import { rateLimiter } from "./middleware/rate-limit.middleware.js";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-  }),
-);
+app.use(securityHeaders);
+app.use(corsOptions);
+app.use(rateLimiter);
+
 app.use(express.json());
 
 app.use("/auth", authRoutes);
@@ -26,39 +23,6 @@ app.get("/", (_, res) => {
   res.json({
     message: "Northstar API running",
   });
-});
-
-app.get("/db-test", async (_, res) => {
-  try {
-    const result = await db.execute("SELECT NOW()");
-
-    res.json(result);
-  } catch (error) {
-    console.error(error);
-
-    res.status(500).json({
-      error: "Database connection failed",
-    });
-  }
-});
-
-app.get("/users", async (_, res) => {
-  const allUsers = await db.select().from(users);
-
-  res.json(allUsers);
-});
-
-app.post("/users", async (req, res) => {
-  const { email, name } = req.body;
-
-  const newUser = await db
-    .insert(users)
-    .values({
-      email,
-      name,
-    } as NewUser)
-    .returning();
-  res.json(newUser);
 });
 
 export default app;
