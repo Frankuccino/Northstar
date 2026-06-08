fd59400d3792aae8dc7aa215aff2196a148f682b
Frank Angelo Malubag <frankabad189@gmail.com>
2026-06-08 23:54:22 +0800

    feat(api): add create employee API function
    - this allows to send a POST request to the backend endpoint /employees

--- northstar-web/src/features/employees/api/employees.api.ts
+++ northstar-web/src/features/employees/api/employees.api.ts
@@ -1,9 +1,18 @@
 import { api } from "@/lib/axios";
 
 import type { Employee } from "../types/employee";
+import type { CreateEmployeeInput } from "../types/employee.types";
 
 export const getEmployees = async (): Promise<Employee[]> => {
   const response = await api("/employees");
+
+  return response.data;
+};
+
+export const createEmployee = async (
+  data: CreateEmployeeInput,
+): Promise<Employee> => {
+  const response = await api.post("/employees", data);
 
   return response.data;
 };
