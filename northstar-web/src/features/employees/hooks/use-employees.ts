e75b9ff15e722f9c6ea9ef552f719d74fc98be8a
Frank Angelo Malubag <frankabad189@gmail.com>
2026-06-10 16:14:23 +0800

    chore: update queryKey on useQuery to use the centralized query key factory

--- northstar-web/src/features/employees/hooks/use-employees.ts
+++ northstar-web/src/features/employees/hooks/use-employees.ts
@@ -1,9 +1,10 @@
+import { employeeKeys } from "../api/employee-query-keys";
 import { getEmployees } from "../api/employees.api";
 import { useQuery } from "@tanstack/react-query";
 
 export const useEmployees = () => {
   return useQuery({
-    queryKey: ["employees"],
+    queryKey: employeeKeys.all,
     queryFn: getEmployees,
   });
 };
