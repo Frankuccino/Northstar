1731b38ad05579be038caca4a3b4e4c12165a7e3
Frank Angelo Malubag <frankabad189@gmail.com>
2026-07-01 22:12:00 +0800

    fix(config): fix the error with the recent commit TS5101

--- northstar-web/src/app/layouts/dashboard-layout.tsx
+++ northstar-web/src/app/layouts/dashboard-layout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { clearToken } from "@/features/auth/utils/token";
 import { useNavigate } from "react-router-dom";
+import { Button } from "@/components/ui/button";
 
 export const DashboardLayout = () => {
   const navigate = useNavigate();
@@ -8,15 +9,15 @@ export const DashboardLayout = () => {
   return (
     <div>
       <nav>Dashboard Navigation</nav>
-      <button
+      <Button
+        variant="destructive"
         onClick={() => {
           clearToken();
           navigate("/login");
         }}
-        className="rounded-lg border border-slate-200 bg-white px-4 py-2 text-sm font-medium text-slate-700 shadow-sm transition-colors duration-200 hover:bg-red-50 hover:text-red-300 focus:outline-none focus:ring-red-500 focus:ring-offset-2"
       >
         Logout
-      </button>
+      </Button>
       <main>
         <Outlet />
       </main>
